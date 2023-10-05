228bb13ebb7aee4d3280544c7cbdad1162dcf30c
lovinaj <lovejon218@gmail.com>
2023-10-05 13:31:12 -0700

    a program that imports functions from a python  file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,4 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     
