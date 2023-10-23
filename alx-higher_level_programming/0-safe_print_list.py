d03360dd2017c482d2ed8614306a145a63d506df
lovinaj <lovejon218@gmail.com>
2023-10-23 05:54:21 -0700

    a function that prints x elements of a list

--- alx-higher_level_programming/0-safe_print_list.py
+++ alx-higher_level_programming/0-safe_print_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            total += 1
+        except IndexError:
+            break
+    print("")
+    return (total);
+
+'''
+def safe_print_list(my_list=[], x=0):
     try:
         # Initialize a count for the number of elements printed
         count = 0
@@ -29,3 +41,4 @@ my_list = [1, 2, 3, 4, 5]
 x = 3
 printed_elements = safe_print_list(my_list, x)
 print(f"Printed {printed_elements} elements from the list.")
+'''
