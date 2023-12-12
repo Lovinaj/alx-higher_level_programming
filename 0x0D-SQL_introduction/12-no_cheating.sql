7b0789ada1a09624cfd940f9262bff4143702610
lovinaj <lovejon218@gmail.com>
2023-12-12 07:06:27 -0800

    sql files

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,4 @@
--- script that updates the record of a table in a database in a MySQL server
-
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+-- update the score of Bob to 10 in the second_table
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
