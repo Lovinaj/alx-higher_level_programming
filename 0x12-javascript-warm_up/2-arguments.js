1855148211a9cb0f385ebc15ff73dcc33a646fbf
lovinaj <lovejon218@gmail.com>
2024-01-08 13:53:04 +0000

    JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.lenght === 2) {
-    console.log('No argument');
-} else if (process.argv.leght === 3) {
-    console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.legth === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Argument found')
+  console.log('Argument found')
 }
