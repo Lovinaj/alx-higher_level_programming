f24b47f8a512e92aab472cd8f5e2bddc7b08e7c5
lovinaj <lovejon218@gmail.com>
2024-01-14 19:14:27 -0800

    orm

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,6 +3,7 @@
    of a State and an instance Base = declarative_base()"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,6 +11,14 @@ Base = declarative_base()
 class State(Base):
     """class State"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
+    id = Column(Integer, unique=True, primary_key=True, nullable=False,
                 autoincrement=True)
     name = Column(String(128), nullable=False)
+
+    # the class attribute cities must represent a relationship
+    # with the class City
+    cities = relationship("City", cascade="all")
+
+    def __str__(self):
+        """String representation"""
+        return ("{}: {}".format(self.id, self.name))
